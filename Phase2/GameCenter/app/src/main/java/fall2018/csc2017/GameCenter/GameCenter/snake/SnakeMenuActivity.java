1fb9556dcd55b0145bcc4bc27afdb5173ddb2d99
shar1178 <ish.sharma@mail.utoronto.ca>
2018-11-18 19:30:27 -0500

    Deleted UALReadWrite activity in android manifest.

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeMenuActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeMenuActivity.java
@@ -4,12 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import fall2018.csc2017.GameCenter.GameCenter.R;
+import fall2018.csc2017.GameCenter.GameCenter.lobby.UserAccount;
 
 public class SnakeMenuActivity extends AppCompatActivity {
+    /**
+     * The current user account obtained from the game select screen.
+     */
+    private UserAccount currentUserAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snake_menu);
+        currentUserAccount =
+                (UserAccount) getIntent().getSerializableExtra("currentUserAccount");
+//        addLoadButtonListener();
+//        addSaveButtonListener();
+//        addLoadAutoSaveButtonListener();
+
     }
 }
