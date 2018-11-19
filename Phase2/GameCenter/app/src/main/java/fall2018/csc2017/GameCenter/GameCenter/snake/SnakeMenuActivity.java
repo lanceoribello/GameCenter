13d4467cc5aa0909184d4b3ac6f11cbda4b4ade3
zhaonancy <nancy.zhao@outlook.com>
2018-11-18 22:24:30 -0500

    Snake now takes in account and updates high score once game is over.

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeMenuActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeMenuActivity.java
@@ -1,6 +1,8 @@
 package fall2018.csc2017.GameCenter.GameCenter.snake;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +10,9 @@ import android.widget.Button;
 
 import fall2018.csc2017.GameCenter.GameCenter.R;
 import fall2018.csc2017.GameCenter.GameCenter.lobby.UserAccount;
+import fall2018.csc2017.GameCenter.GameCenter.slidingtiles.SlidingTilesBoardManager;
 import fall2018.csc2017.GameCenter.GameCenter.slidingtiles.activities.SlidingTilesGameActivity;
+import fall2018.csc2017.GameCenter.GameCenter.slidingtiles.activities.SlidingTilesMenuActivity;
 import fall2018.csc2017.GameCenter.GameCenter.snake.activities.SnakeStartingActivity;
 
 public class SnakeMenuActivity extends AppCompatActivity {
@@ -26,18 +30,43 @@ public class SnakeMenuActivity extends AppCompatActivity {
 //        addLoadButtonListener();
 //        addSaveButtonListener();
 //        addLoadAutoSaveButtonListener();
-          addNewGameListener();
     }
-    private void addNewGameListener(){
-        Button newGame = findViewById(R.id.NewSnake);
-        newGame.setOnClickListener(new View.OnClickListener(){
+
+    /**
+     * Switch to SnakeStartingActivity to play the game.
+     */
+    private void switchToGame(String difficulty) {
+        Intent intent = new Intent(this, SnakeStartingActivity.class);
+        intent.putExtra("currentUserAccount", currentUserAccount);
+        intent.putExtra("difficulty", difficulty);
+        startActivity(intent);
+    }
+
+    /**
+     * Activate the start button. Once the start button is pressed, a new alert dialog
+     * prompts the user to choose between the 2 difficulties: easy, hard.
+     *
+     * @param view the current view.
+     */
+    public void newGame(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                SnakeMenuActivity.this);
+        builder.setTitle("Choose a Complexity");
+        String[] difficulties = {"Easy", "Hard"};
+        builder.setItems(difficulties, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent tmp = new Intent(getApplicationContext(), SnakeStartingActivity.class);
-                tmp.putExtra("currentUserAccount", currentUserAccount);
-                startActivity(tmp);
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        switchToGame("easy");
+                        break;
+                    case 1:
+                        switchToGame("hard");
+                        break;
+                }
             }
         });
-
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
