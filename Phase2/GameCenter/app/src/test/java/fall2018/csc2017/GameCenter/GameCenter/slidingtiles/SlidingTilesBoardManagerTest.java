5f5b0d9fdc4086172f2c0d6e707ceb81b13fcde4
Neel <neelu.patel@mail.utoronto.ca>
2018-11-23 20:29:44 -0500

    Created unit test files for Board, Boardmanager and Tile

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/SlidingTilesBoardManagerTest.java
@@ -0,0 +1,40 @@
+package fall2018.csc2017.GameCenter.GameCenter.slidingtiles;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SlidingTilesBoardManagerTest {
+
+    @Test
+    public void getSavedBoards() {
+    }
+
+    @Test
+    public void getComplexity() {
+    }
+
+    @Test
+    public void getBoard() {
+    }
+
+    @Test
+    public void puzzleSolved() {
+    }
+
+    @Test
+    public void isValidTap() {
+    }
+
+    @Test
+    public void touchMove() {
+    }
+
+    @Test
+    public void getMoves() {
+    }
+
+    @Test
+    public void undo() {
+    }
+}
