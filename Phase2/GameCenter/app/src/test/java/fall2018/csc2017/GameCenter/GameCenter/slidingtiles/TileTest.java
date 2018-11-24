8d843fc4a4943fc8dc62014c1fb18714637a1727
Neel <neelu.patel@mail.utoronto.ca>
2018-11-23 21:01:06 -0500

    Created unit tests for the tile class

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/TileTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/TileTest.java
@@ -6,15 +6,46 @@ import static org.junit.Assert.*;
 
 public class TileTest {
 
-    @Test
-    public void getBackground() {
-    }
+    /**
+     * The Tile for testing, creates a tile with id 4 set to the background image of 4 in drawables
+     */
+
+    Tile tile = new Tile(4, 2131099791);
+
+    /**
+     * Checks if the tile returns the correct background based on its id
+     */
 
     @Test
-    public void getId() {
+    public void getBackgroundIsCorrect() {
+        int tile4BackgroundId = 2131099791; // The resource id for the tile image representing 4
+        assertEquals(tile4BackgroundId, tile.getBackground());
+
     }
 
+    /**
+     * Checks if the tile returns the correct id
+     */
     @Test
-    public void compareTo() {
+    public void getIdIsCorrect() {
+        int tile4Id = 4; // The id which a tile 4 should contain
+        assertEquals(tile4Id, tile.getId());
+    }
+
+    /**
+     * Checks if two tiles with different IDs compare as different tiles
+     */
+    @Test
+    public void compareToDifferentTile() {
+        Tile tileCompare = new Tile(5, 2131099792);
+        assertNotSame(tileCompare, tile);
+    }
+
+    /**
+     * Checks if two of the same tiles compare as the same
+     */
+    @Test
+    public void compareToSameTile() {
+        assertSame(tile, tile);
     }
 }
