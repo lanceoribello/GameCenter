ba6a66d1280fdb614f651f624d9f57609e4e32c3
Neel <neelu.patel@mail.utoronto.ca>
2018-11-24 14:19:47 -0500

    Added mockito testing suite and changes to board unit tests

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/BoardTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/BoardTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.content.Context;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.when;
 
 
 import fall2018.csc2017.GameCenter.GameCenter.slidingtiles.activities.SlidingTilesMenuActivity;
@@ -15,21 +23,40 @@ public class BoardTest {
     /**
      * Sliding tiles starting activity used for our test board
      */
-    SlidingTilesMenuActivity menu = new SlidingTilesMenuActivity();
+    private SlidingTilesMenuActivity menu = new SlidingTilesMenuActivity();
 
     /**
-     * Board manager used to initialize 5x5 board
+     * Number of tiles on a board, 9 for 3x3, 16 for 4x4, 25 for 5x5
      */
-    SlidingTilesBoardManager boardManager = new
-            SlidingTilesBoardManager(5, menu.getTileIdList(5));
+    private int numTiles;
+
+    /**
+     * List of tiles used for our board, in row major order
+     */
+    private List<Tile> setTileList() {
+        List<Tile> tiles = new ArrayList<>();
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(tileNum, tileIdList.get(tileNum)));
+        }
+        return tiles;
+    }
+
+    /**
+     * Background IDs in row-major depending on numTiles
+     */
+    ArrayList<Integer> tileIdList = menu.getTileIdList((int)Math.sqrt(numTiles));
+
 
     /**
      * Board used to be tested
      */
-    private Board board = boardManager.getBoard();
+    private Board board = new Board(setTileList());
 
     @Test
-    public void getTiles() {
+    public void getTilesIsCorrect3x3() {
+        numTiles = 9;
+
+
     }
 
     @Test
