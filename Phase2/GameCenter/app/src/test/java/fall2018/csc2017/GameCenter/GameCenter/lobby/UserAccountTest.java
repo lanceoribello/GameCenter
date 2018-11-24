c808dddfdb8ed763819f10442e2eb55f7aafd1ab
zhaonancy <nancy.zhao@outlook.com>
2018-11-24 13:26:08 -0500

    Fixed Sliding Tiles Menu - before it was pulling game names of Snake, not Sliding Tiles. User Account Tests in progress.

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/lobby/UserAccountTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/lobby/UserAccountTest.java
@@ -2,6 +2,9 @@ package fall2018.csc2017.GameCenter.GameCenter.lobby;
 
 import org.junit.Test;
 
+import fall2018.csc2017.GameCenter.GameCenter.slidingtiles.SlidingTilesBoardManager;
+import fall2018.csc2017.GameCenter.GameCenter.slidingtiles.activities.SlidingTilesMenuActivity;
+
 import static org.junit.Assert.*;
 
 public class UserAccountTest {
@@ -30,44 +33,65 @@ public class UserAccountTest {
     }
 
     /**
+     * Tests if updated high score is saved correctly.
+     */
+    @Test
+    public void setTopScore() {
+        String gameLevel = "Sliding Tiles 3x3";
+        Integer topScore = 42;
+        tester.setTopScore(gameLevel, topScore);
+        assertEquals(topScore, tester.getTopScore(gameLevel));
+    }
+
+    /**
      * Tests if updated high score is returned correctly.
      */
     @Test
     public void getTopScore() {
         String gameLevel = "Sliding Tiles 3x3";
-        int topScore = 42;
-        tester.setTopScore(gameLevel, 42);
-        //assertEquals(topScore, tester.getTopScore(gameLevel));
+        Integer topScore = 42;
+        assertEquals(topScore, tester.getTopScore(gameLevel));
     }
 
     /**
-     * Tests if updated high score is saved correctly.
+     * Tests if Sliding Tiles game is added correctly.
      */
     @Test
-    public void setTopScore() {
-
-    }
-
-    @Test
     public void addSlidingTilesGame() {
+        //String gameName = "Saved Game";
     }
 
+    /**
+     * Tests if Sliding Tiles game is returned correctly.
+     */
     @Test
     public void getSlidingTilesGame() {
     }
 
+    /**
+     * Tests if Sliding Tiles game names are returned correctly.
+     */
     @Test
     public void getSlidingTilesGameNames() {
     }
 
+    /**
+     * Tests if Snake game is added correctly.
+     */
     @Test
     public void addSnakeGame() {
     }
 
+    /**
+     * Tests if Snake game is returned correctly.
+     */
     @Test
     public void getSnakeGame() {
     }
 
+    /**
+     * Tests if Snake game names are returned correctly.
+     */
     @Test
     public void getSnakeGameNames() {
     }
