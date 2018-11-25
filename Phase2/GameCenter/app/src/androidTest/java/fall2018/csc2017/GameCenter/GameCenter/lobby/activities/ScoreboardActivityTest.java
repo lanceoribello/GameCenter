d4d4e4ad13581cbc703cd0c0293af4eb957c9050
zhaonancy <nancy.zhao@outlook.com>
2018-11-24 19:19:29 -0500

    Configured activity testing for game select and scoreboard

--- Phase2/GameCenter/app/src/androidTest/java/fall2018/csc2017/GameCenter/GameCenter/lobby/activities/ScoreboardActivityTest.java
+++ Phase2/GameCenter/app/src/androidTest/java/fall2018/csc2017/GameCenter/GameCenter/lobby/activities/ScoreboardActivityTest.java
@@ -1,10 +1,19 @@
 package fall2018.csc2017.GameCenter.GameCenter.lobby.activities;
 
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
 
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class ScoreboardActivityTest {
+
+    @Rule
+    public ActivityTestRule<ScoreboardActivity> scoreboardActivityRule =
+            new ActivityTestRule<>(ScoreboardActivity.class);
 
     @Test
     public void onCreate() {
