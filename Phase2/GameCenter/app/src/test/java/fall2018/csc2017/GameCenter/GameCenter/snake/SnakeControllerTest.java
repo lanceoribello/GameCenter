274e40bd593da77ad94b496c306ded8ef21c6a81
shar1178 <ish.sharma@mail.utoronto.ca>
2018-11-28 17:33:02 -0500

    Addin a test for SnakeController

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeControllerTest.java
@@ -0,0 +1,76 @@
+package fall2018.csc2017.GameCenter.GameCenter.snake;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SnakeControllerTest {
+
+    @Test
+    public void detectDeath() {
+    }
+
+    @Test
+    public void createSaveData() {
+    }
+
+    @Test
+    public void getAutoSaveData() {
+    }
+
+    @Test
+    public void getSavePoint() {
+    }
+
+    @Test
+    public void updateGame() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void getAppleX() {
+    }
+
+    @Test
+    public void getAppleY() {
+    }
+
+    @Test
+    public void getBombX() {
+    }
+
+    @Test
+    public void getBombY() {
+    }
+
+    @Test
+    public void getSnakeXs() {
+    }
+
+    @Test
+    public void getSnakeYs() {
+    }
+
+    @Test
+    public void getSnakeLength() {
+    }
+
+    @Test
+    public void getDirection() {
+    }
+
+    @Test
+    public void setDirection() {
+    }
+
+    @Test
+    public void getFps() {
+    }
+
+    @Test
+    public void setAutoSaveData() {
+    }
+}
