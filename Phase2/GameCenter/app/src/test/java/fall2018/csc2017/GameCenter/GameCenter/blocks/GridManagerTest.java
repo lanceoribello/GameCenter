eb4a2df7bc489c9b0e0ecc8f75098456c3e930e9
zhaonancy <nancy.zhao@outlook.com>
2018-11-27 22:00:20 -0500

    Created GridManager test class

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/GameCenter/blocks/GridManagerTest.java
@@ -0,0 +1,36 @@
+package fall2018.csc2017.GameCenter.GameCenter.blocks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GridManagerTest {
+
+    @Test
+    public void getSavedGrids() {
+    }
+
+    @Test
+    public void getGrid() {
+    }
+
+    @Test
+    public void setGrid() {
+    }
+
+    @Test
+    public void gameOver() {
+    }
+
+    @Test
+    public void placeBlock() {
+    }
+
+    @Test
+    public void undo() {
+    }
+
+    @Test
+    public void movePlayer() {
+    }
+}
