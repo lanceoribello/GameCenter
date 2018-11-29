e96c0f828afc5639f597c21b932136b2ca7cce17
Lance <lance.oribello@mail.utoronto.ca>
2018-11-29 01:05:47 -0500

    Minor fixes in SnakeMenuController

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeMenuController.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/snake/SnakeMenuController.java
@@ -5,14 +5,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 import fall2018.csc2017.GameCenter.GameCenter.lobby.UserAccount;
-import fall2018.csc2017.GameCenter.GameCenter.lobby.activities.LoginActivity;
 
 public class SnakeMenuController {
     /**
      * Update the user Account List.
      */
     public static void updateUserAccounts(UserAccount ua, Object[] savedData, ArrayList<UserAccount>
-                                          ual) {
+            ual) {
         Calendar c = Calendar.getInstance();
         DateFormat dateFormat =
                 DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
@@ -25,7 +24,6 @@ public class SnakeMenuController {
 
     /**
      * Make a list of games names for displaying in load games.
-
      */
     public static String[] savedGamesList(UserAccount ua) {
         String[] games = new String[(ua.getSnakeGameNames().size())];
