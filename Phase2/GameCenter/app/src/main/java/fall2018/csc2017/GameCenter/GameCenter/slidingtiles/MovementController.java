3d96fd3ff99b83eb0a589f55b97d02feb11027a9
Lance <lance.oribello@mail.utoronto.ca>
2018-11-29 00:49:08 -0500

    Minor fixes and docstring in MovementController

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/MovementController.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/GameCenter/GameCenter/slidingtiles/MovementController.java
@@ -3,18 +3,31 @@ package fall2018.csc2017.GameCenter.GameCenter.slidingtiles;
 import android.content.Context;
 import android.widget.Toast;
 
-public class MovementController {
+/**
+ * Processes the tapping of moves in Sliding Tiles.
+ */
+class MovementController {
 
+    /**
+     * The boardManager of this MovementController.
+     */
     private BoardManager boardManager = null;
 
-    public MovementController() {
-    }
-
-    public void setBoardManager(BoardManager boardManager) {
+    /**
+     * Sets the boardManager to the given boardManager
+     * @param boardManager the given boardManager
+     */
+    void setBoardManager(BoardManager boardManager) {
         this.boardManager = boardManager;
     }
 
-    public void processTapMovement(Context context, int position, boolean display) {
+    /**
+     * Processes the tapping of moves in Sliding Tiles.
+     * @param context the context of the activity
+     * @param position the position of the tap
+     * @param display whether the display is shown
+     */
+    void processTapMovement(Context context, int position, boolean display) {
         if (boardManager.isValidTap(position)) {
             boardManager.touchMove(position);
             if (boardManager.puzzleSolved()) {
